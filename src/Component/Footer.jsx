83a380865a9146465018c65721720feaf217ebfc
unknown <ajoy9532@gmail.com>
2023-07-14 14:43:08 +0600

    added footer

---
+++ src/Component/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <h1 className="text-center font-Montserrat mt-8 mb-4 font-medium text-[#828282] ">
+      created by Maksudur Rahman - devChallenges.io
+    </h1>
+  );
+};
+
+export default Footer;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,20 @@
 import LeftSide from "./Component/LeftSide";
 import RightSide from "./Component/RightSide";
+import Footer from "./Component/footer";
 
 const Home = () => {
   return (
-    <div className="p-4 lg:flex">
-      <div>
-        <LeftSide />
+    <>
+      <div className="p-4 lg:flex">
+        <div>
+          <LeftSide />
+        </div>
+        <div>
+          <RightSide />
+        </div>
       </div>
-      <div>
-        <RightSide />
-      </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
