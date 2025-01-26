918f3d468bd5c3f9a8232fe9ad98b0d9b4b0600a
DESKTOP-4E9T2MH\Admin <ajoy9532@gmail.com>
2025-01-26 22:58:56 +0600

    updated

--- src/Component/Skills.jsx
+++ src/Component/Skills.jsx
@@ -12,6 +12,8 @@ import { TbBrandVscode } from "react-icons/tb";
 import jwt from "../assets/jwt.svg";
 import TanStack from "../assets/TanStack.png";
 import daisyui from "../assets/daisyui.png";
+import problem_solving from "../assets/Problem-solving.png";
+import gsap from "../assets/gsap.jpg";
 import {
   SiTailwindcss,
   SiReactrouter,
@@ -31,8 +33,8 @@ const Skills = () => {
       <h3 className="font-bold text-[#4F4F4F] text-lg uppercase mt-2 text-left ">
         Skills
       </h3>
-      <div className="mt-4">
-        {/* {skills.map((skill) => (
+
+      {/* {skills.map((skill) => (
           <div className="container flex mt-4" key={skill.name}>
             <div className="w-1/4 text-left sm:text-sm">
               <span className="mr-8 text-left text-[#4F4F4F] font-medium">
@@ -48,162 +50,175 @@ const Skills = () => {
             </div>
           </div>
         ))} */}
-        <div className="grid grid-cols-2 p-6 lg:p-0 lg:grid-cols-6 gap-x-14 gap-y-4">
-          <div className="flex gap-1 items-center">
-            <div>
-              <FaHtml5 className="w-8 h-8 text-red-600"></FaHtml5>
-            </div>
-
-            <h1 className="text-[#4F4F4F] font-medium">HTML</h1>
-          </div>
-          <div className="flex gap-1 items-center">
-            <div>
-              <FaCss3Alt className="w-8 h-8 text-blue-600"></FaCss3Alt>
-            </div>
-
-            <h1 className="text-[#4F4F4F] font-medium">CSS</h1>
-          </div>
-          <div className="flex gap-1 items-center">
-            <div>
-              <SiTailwindcss className="w-8 h-8 text-cyan-500"></SiTailwindcss>
-            </div>
-
-            <h1 className="text-[#4F4F4F] font-medium">Tailwind</h1>
-          </div>
-          <div className="flex gap-1 items-center">
-            <div>
-              <FaBootstrap className="w-8 h-8 text-[#7F11F9]"></FaBootstrap>
-            </div>
-
-            <h1 className="text-[#4F4F4F] font-medium">Bootstrap</h1>
-          </div>
-          <div className="flex gap-1 items-center">
-            <div>
-              <img src={daisyui} className="w-8 h-8 " alt="" />
-            </div>
-
-            <h1 className="text-[#4F4F4F] font-medium">DaisyUI</h1>
-          </div>
-          <div className="flex gap-1 items-center">
-            <div>
-              <TbBrandJavascript className="w-8 h-8 text-yellow-500"></TbBrandJavascript>
-            </div>
-
-            <h1 className="text-[#4F4F4F] font-medium">Javascript</h1>
-          </div>
-          <div className="flex gap-1 items-center">
-            <div>
-              <FaReact className="w-8 h-8 text-rose-600"></FaReact>
-            </div>
-
-            <h1 className="text-[#4F4F4F] font-medium">React</h1>
-          </div>
-          <div className="flex gap-1 items-center">
-            <div>
-              <FaVuejs className="w-8 h-8 text-[#3FB27F]"></FaVuejs>
-            </div>
-
-            <h1 className="text-[#4F4F4F] font-medium">Vue JS</h1>
+      <div className="grid grid-cols-2 p-6 lg:p-2 lg:grid-cols-6  gap-y-4 mt-4">
+        <div className="flex gap-1 items-center">
+          <div>
+            <FaHtml5 className="w-8 h-8 text-red-600"></FaHtml5>
           </div>
 
-          <div className="flex gap-1 items-center">
-            <div>
-              <SiReactrouter className="w-8 h-8"></SiReactrouter>
-            </div>
-
-            <h1 className="text-[#4F4F4F] font-medium">React Router</h1>
+          <h1 className="text-[#4F4F4F] font-medium">HTML</h1>
+        </div>
+        <div className="flex gap-1 items-center">
+          <div>
+            <FaCss3Alt className="w-8 h-8 text-blue-600"></FaCss3Alt>
           </div>
-          <div className="flex gap-1 items-center">
-            <div className="rounded-full">
-              <FaNodeJs className="w-8 h-8  text-green-600"></FaNodeJs>
-            </div>
 
-            <h1 className="text-[#4F4F4F] font-medium">Node JS</h1>
+          <h1 className="text-[#4F4F4F] font-medium">CSS</h1>
+        </div>
+        <div className="flex gap-1 items-center">
+          <div>
+            <SiTailwindcss className="w-8 h-8 text-cyan-500"></SiTailwindcss>
           </div>
-          <div className="flex gap-1 items-center">
-            <div className="rounded-full">
-              <SiMongodb className="w-8 h-8  text-green-600"></SiMongodb>
-            </div>
 
-            <h1 className="text-[#4F4F4F] font-medium">MongoDB</h1>
+          <h1 className="text-[#4F4F4F] font-medium">Tailwind</h1>
+        </div>
+        <div className="flex gap-1 items-center">
+          <div>
+            <FaBootstrap className="w-8 h-8 text-[#7F11F9]"></FaBootstrap>
           </div>
-          <div className="flex gap-1 items-center">
-            <div className="rounded-full">
-              <SiExpress className="w-8 h-8  text-black"></SiExpress>
-            </div>
 
-            <h1 className="text-[#4F4F4F] font-medium">Express JS</h1>
+          <h1 className="text-[#4F4F4F] font-medium">Bootstrap</h1>
+        </div>
+        <div className="flex gap-1 items-center">
+          <div>
+            <img src={daisyui} className="w-8 h-8 " alt="" />
           </div>
-          <div className="flex gap-1 items-center">
-            <div className="rounded-full">
-              <img src={jwt} className="w-8 h-8  text-black" alt="" />
-            </div>
 
-            <h1 className="text-[#4F4F4F] font-medium">JWT</h1>
+          <h1 className="text-[#4F4F4F] font-medium">DaisyUI</h1>
+        </div>
+        <div className="flex gap-1 items-center">
+          <div>
+            <TbBrandJavascript className="w-8 h-8 text-yellow-500"></TbBrandJavascript>
           </div>
-          <div className="flex gap-1 items-center">
-            <div className="rounded-full">
-              <img src={TanStack} className="w-8 h-8  text-black" alt="" />
-            </div>
 
-            <h1 className="text-[#4F4F4F] font-medium">TanStack Query</h1>
+          <h1 className="text-[#4F4F4F] font-medium">Javascript</h1>
+        </div>
+        <div className="flex gap-1 items-center">
+          <div>
+            <FaReact className="w-8 h-8 text-rose-600"></FaReact>
           </div>
-          <div className="flex gap-1 items-center">
-            <div className="rounded-full">
-              <SiFirebase className="w-8 h-8  text-amber-500"></SiFirebase>
-            </div>
 
-            <h1 className="text-[#4F4F4F] font-medium">Firebase</h1>
+          <h1 className="text-[#4F4F4F] font-medium">React</h1>
+        </div>
+        <div className="flex gap-1 items-center">
+          <div>
+            <FaVuejs className="w-8 h-8 text-[#3FB27F]"></FaVuejs>
           </div>
-          <div className="flex gap-1 items-center">
-            <div className="rounded-full">
-              <SiAxios className="w-8 h-8  text-[#AC93F1]"></SiAxios>
-            </div>
 
-            <h1 className="text-[#4F4F4F] font-medium">AXIOS</h1>
-          </div>
-          <div className="flex gap-1 items-center">
-            <div className="rounded-full">
-              <TbBrandVscode className="w-8 h-8  text-blue-800"></TbBrandVscode>
-            </div>
+          <h1 className="text-[#4F4F4F] font-medium">Vue JS</h1>
+        </div>
 
-            <h1 className="text-[#4F4F4F] font-medium">VS Code</h1>
+        <div className="flex gap-1 items-center">
+          <div>
+            <SiReactrouter className="w-8 h-8"></SiReactrouter>
           </div>
-          <div className="flex gap-1 items-center">
-            <div className="rounded-full">
-              <FaGithub className="w-8 h-8  text-black"></FaGithub>
-            </div>
 
-            <h1 className="text-[#4F4F4F] font-medium">Github </h1>
+          <h1 className="text-[#4F4F4F] font-medium">React Router</h1>
+        </div>
+        <div className="flex gap-1 items-center">
+          <div className="rounded-full">
+            <FaNodeJs className="w-8 h-8  text-green-600"></FaNodeJs>
           </div>
-          <div className="flex gap-1 items-center">
-            <div className=" rounded-full">
-              <SiEslint className="w-8 h-8  text-[#4930BD]"></SiEslint>
-            </div>
 
-            <h1 className="text-[#4F4F4F] font-medium">Eslint </h1>
+          <h1 className="text-[#4F4F4F] font-medium">Node JS</h1>
+        </div>
+        <div className="flex gap-1 items-center">
+          <div className="rounded-full">
+            <SiMongodb className="w-8 h-8  text-green-600"></SiMongodb>
           </div>
-          <div className="flex gap-1 items-center">
-            <div className=" rounded-full">
-              <FaFigma className="w-8 h-8  text-[#4930BD]"></FaFigma>
-            </div>
 
-            <h1 className="text-[#4F4F4F] font-medium">Figma </h1>
+          <h1 className="text-[#4F4F4F] font-medium">MongoDB</h1>
+        </div>
+        <div className="flex gap-1 items-center">
+          <div className="rounded-full">
+            <SiExpress className="w-8 h-8  text-black"></SiExpress>
           </div>
-          <div className="flex gap-1 items-center">
-            <div className="rounded-full">
-              <SiNetlify className="w-8 h-8  text-[#0ABEBB]"></SiNetlify>
-            </div>
 
-            <h1 className="text-[#4F4F4F] font-medium">Netlify </h1>
+          <h1 className="text-[#4F4F4F] font-medium">Express JS</h1>
+        </div>
+        <div className="flex gap-1 items-center">
+          <div className="rounded-full">
+            <img src={jwt} className="w-8 h-8  text-black" alt="" />
           </div>
-          <div className="flex gap-1 items-center">
-            <div className="rounded-full">
-              <SiVercel className="w-8 h-8  text-black"></SiVercel>
-            </div>
 
-            <h1 className="text-[#4F4F4F] font-medium">Vercel </h1>
+          <h1 className="text-[#4F4F4F] font-medium">JWT</h1>
+        </div>
+        <div className="flex gap-1 items-center">
+          <div className="rounded-full">
+            <img src={TanStack} className="w-8 h-8  text-black" alt="" />
           </div>
+
+          <h1 className="text-[#4F4F4F] font-medium">React Query</h1>
+        </div>
+        <div className="flex gap-1 items-center">
+          <div className="rounded-full">
+            <img src={gsap} className="w-8 h-8  text-black" alt="" />
+          </div>
+
+          <h1 className="text-[#4F4F4F] font-medium">Gsap</h1>
+        </div>
+        <div className="flex gap-1 items-center">
+          <div className="rounded-full">
+            <SiFirebase className="w-8 h-8  text-amber-500"></SiFirebase>
+          </div>
+
+          <h1 className="text-[#4F4F4F] font-medium">Firebase</h1>
+        </div>
+        <div className="flex gap-1 items-center">
+          <div className="rounded-full">
+            <SiAxios className="w-8 h-8  text-[#AC93F1]"></SiAxios>
+          </div>
+
+          <h1 className="text-[#4F4F4F] font-medium">AXIOS</h1>
+        </div>
+        <div className="flex gap-1 items-center">
+          <div className="rounded-full">
+            <TbBrandVscode className="w-8 h-8  text-blue-800"></TbBrandVscode>
+          </div>
+
+          <h1 className="text-[#4F4F4F] font-medium">VS Code</h1>
+        </div>
+        <div className="flex gap-1 items-center">
+          <div className="rounded-full">
+            <FaGithub className="w-8 h-8  text-black"></FaGithub>
+          </div>
+
+          <h1 className="text-[#4F4F4F] font-medium">Github </h1>
+        </div>
+        <div className="flex gap-1 items-center">
+          <div className=" rounded-full">
+            <SiEslint className="w-8 h-8  text-[#4930BD]"></SiEslint>
+          </div>
+
+          <h1 className="text-[#4F4F4F] font-medium">Eslint </h1>
+        </div>
+        <div className="flex gap-1 items-center">
+          <div className=" rounded-full">
+            <FaFigma className="w-8 h-8  text-[#4930BD]"></FaFigma>
+          </div>
+
+          <h1 className="text-[#4F4F4F] font-medium">Figma </h1>
+        </div>
+        <div className="flex gap-1 items-center">
+          <div className="rounded-full">
+            <SiNetlify className="w-8 h-8  text-[#0ABEBB]"></SiNetlify>
+          </div>
+
+          <h1 className="text-[#4F4F4F] font-medium">Netlify </h1>
+        </div>
+        <div className="flex gap-1 items-center">
+          <div className="rounded-full">
+            <SiVercel className="w-8 h-8  text-black"></SiVercel>
+          </div>
+
+          <h1 className="text-[#4F4F4F] font-medium">Vercel </h1>
+        </div>
+        <div className="flex gap-1 items-center">
+          <div className="rounded-full">
+            <img src={problem_solving} className="w-8 h-8  text-black" alt="" />
+          </div>
+
+          <h1 className="text-[#4F4F4F] font-medium">Prob Solv</h1>
         </div>
       </div>
     </div>
